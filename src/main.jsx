a8953aa5eb59a597e3d623a70a4232bbd3783d4e
sujalspecta <sujalspectaclicks@outlook.com>
2026-09-22 14:54:03 +0530

    complete the web

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client"; 
 import App from './main-component/App/App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/react-modal-video/scss/modal-video.scss';
@@ -14,10 +15,13 @@ import './css/custom-font.css';
 import './css/main.css';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+
+const root = createRoot(document.getElementById("root"));
+ root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+ );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
